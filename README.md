728d314a37be81dadf0cf0d663de851df7e91a51
Dimitri Dias Moreira <taikamya@outlook.com>
2022-08-08 12:46:32 +0200

    second commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Price calculator for the current energy price laws adopted in Spain.
+
+Gives how much money the device costs you. In Euros
--- main.py
+++ main.py
@@ -3,8 +3,11 @@
 '''Just a price calculator in Euros for the current energy price laws adopted in Spain'''
 
 HEPH = 0.40739  # Highest Energy Price per hour
+AEPH = 0.25861  # Average Energy Price per hour
 
 def calc_en() -> str:
+    '''E(kWh/day) = P(W) * t(h/day) / 1000(W/kW)
+    Returns the consumption in Euros.'''
     # kWh
     potency = int(input("Cual es la potencia tu electrodoméstico? **Como pone en la etiqueta por favor (kWh/1000h)** \n"))
     
@@ -13,6 +16,7 @@ def calc_en() -> str:
     
     # potency/1000h = Wh
     kwh = potency / 1000
+    print(kwh)
     
     # Wh * price per hour
     kwh_hour = kwh * HEPH
